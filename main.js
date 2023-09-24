47eeeba49082aeabb23b3802276619ca41e25bf9
otaviozerotwo <deadplatypus@protonmail.com>
2023-09-24 18:22:56 -0300

    aumento valor var limiteRolagem arquivo main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     //pega o elemento com o id especificado no html
     var $botaoFlutuanteHeader = $('#btn-scroll-header');
     //define um limite de rolagem para mostrar o botão
-    var limiteRolagem = 250;
+    var limiteRolagem = 300;
     //verificar a posição de rolagem da página
     $(window).scroll(function(){
         if($(this).scrollTop() > limiteRolagem){
