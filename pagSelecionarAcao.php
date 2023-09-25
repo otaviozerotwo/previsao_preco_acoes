108e99cee71e2c1a92b2add01dd5de9bb905ac49
fabiano.pessoal@yahoo.com.br <fabiano.pessoal@yahoo.com.br>
2023-09-24 22:27:27 -0300

    correções

--- pagSelecionarAcao.php
+++ pagSelecionarAcao.php
@@ -1,5 +1,5 @@
 <?php
-    include_once("header.php");
+include_once("header.php");
 ?>
 
 <main class="w-100 m-auto">
@@ -14,45 +14,43 @@
                 <div class="col-md-6 offset-md-3">
                     <label for="codigo" class="h4">SELECIONE A AÇÃO</label>
                     <select name="codigo" class="form-select">
-                        <option style="color: red;">CMIG4</option>
-                        <option style="color: red;">ITUB4</option>
+                        <option style="color: green;">VALE3</option>
+                        <option style="color: green;">PETR4</optipn>
+                        <option style="color: green;">ITUB4</option>
+                        <option style="color: green;">PETR3</option>
+                        <option style="color: green;">BBDC4</option>
+                        <option style="color: green;">B3SA3</option>
+                        <option style="color: green;">BRAS3</option>
+                        <option style="color: green;">ELET3</option>
+                        <optipn style="color: red;">BRAD4</option>
+                        <option style="color: red;">LOCA3</option>
                         <option style="color: red;">BBAS3</option>
-                        <option style="color: red;">VALE3</option>
-                        <optipn>PETR4</optipn>
-                        <optipn>ITAU4</optipn>
-                        <option>PETR3</option>
-                        <optipn>BRAD4</optipn>
-                        <option>ELET3</option>
-                        <option>BRAS3</option>
-                        <option>LOCA3</option>
-                        <optipn>ITAU4</optipn>
-                        <option>PETR3</option>
-                        <optiunt style="color: red;">BTGP11</optiunt>
+                        <option style="color: red;">BTGP11</option>
                         <option style="color: red;">EQUA3</option>
                         <option style="color: red;">RAIA3</option>
                         <option style="color: red;">SUZA3</option>
                         <option style="color: red;">REDE3</option>
-                        <optipn style="color: red;">GERD4</optipn>
-                        <option style="color: red;">RUMO3</option>
-                        <option style="color: red;">BBSE3</option>
-                        <option style="color: red;">VIBR3</option>
-                        <option style="color: red;">COSA3</option>
-                        <option style="color: red;">ULTR3</option>
-                        <option style="color: red;">SABE3</option>
-                        <option style="color: red;">BRAD3</option>
-                        <option style="color: red;">HAPV3</option>
-                        <option style="color: red;">ENEV3</option>
-                        <option style="color: red;">TELE3</option>
-                        <optipn style="color: red;">CEMI4</optipn>
+                        <option style="color: blue">GERD4</optipn>
+                        <option style="color: blue">RUMO3</option>
+                        <option style="color: blue">VIBR3</option>
+                        <option style="color: blue">COSA3</option>
+                        <option style="color: blue">BBSE3</option>
+                        <option style="color: blue">ULTR3</option>
+                        <option style="color: blue">SABE3</option>
+                        <option style="color: blue">BRAD3</option>
+                        <option style="color: blue">HAPV3</option>
+                        <option style="color: blue">ENEV3</option>
+                        <option style="color: blue">TELE3</option>
+                        <option style="color: blue">CMIG4</option>
                         <option style="color: red;">ASSA3</option>
                         <option style="color: red;">HYPE3</option>
-                        <optiunt style="color: red;">KLAB11</optiunt>
+                        <option style="color: red;">KLAB11</option>
                         <option style="color: red;">BRF 3</option>
                         <option style="color: red;">LOJA3</option>
-                        <optipnb style="color: red;">COPE6</optipnb>
+                        <option style="color: red;">COPE6</option>
                         <option style="color: red;">TOTV3</option>
                         <option style="color: red;">EMBR3</option>
-                        <optiunt style="color: red;">ENER11</optiunt>
+                        <option style="color: red;">ENER11</option>
                         <option style="color: red;">GRUP3</option>
                         <option style="color: red;">CCR 3</option>
                         <option style="color: red;">ALIA3</option>
@@ -70,5 +68,5 @@
 </main>
 
 <?php
-    include_once("footer.php");
+include_once("footer.php");
 ?>
